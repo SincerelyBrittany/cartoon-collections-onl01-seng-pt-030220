b9bdf7d9ad34ca17832cf3a5dcd5ef6bdbcc030e
Brittany Russell <sincerelybr@gmail.com>
2020-03-12 23:10:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,13 +17,9 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.each_with_index{ |item, index| 
-  items = item.length}
+  array.each_with_index{ |item, index| items = item.length
   if any? items == 4
-    false
-  else
-    true
-  end
+  end}
 end
 
 def find_the_cheese# code an argument here
