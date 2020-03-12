e8b1cfb2261e1c2a01ae026e2618d85b9f84eb09
Brittany Russell <sincerelybr@gmail.com>
2020-03-12 23:56:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_length = cheese_types.length
-  
+  cheese_total = cheese_types.each { |n| }
   if array.include? cheese_types[0]
     puts cheese_types[0]
   elsif array.include? cheese_types[1]
