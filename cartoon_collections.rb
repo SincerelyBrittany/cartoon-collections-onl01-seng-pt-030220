9d5b39f310ba8b2b1153b91ec3f74352c51db7ac
Brittany Russell <sincerelybr@gmail.com>
2020-03-12 23:46:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  aeeary.include? "cheddar"
+  array.include? "cheddar"
 end
 
 puts find_the_cheese(array)
