db4242047967001490511b82ecdc1a57efb7a664
Brittany Russell <sincerelybr@gmail.com>
2020-03-12 22:47:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+array = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index{ |item, index| 
+  index += 1 
+  puts "#{index} #{item}" }
+  
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  new_array = []
+  array.collect {|name| new_array << name.capitalize + "!" }
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? [{|word| words = word.length puts words}] 
+  
+  
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+puts long_planeteer_calls(array)
