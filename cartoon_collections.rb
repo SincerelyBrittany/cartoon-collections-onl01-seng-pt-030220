058e2ae607362ccff515dbd6c8d27326aad25a27
Brittany Russell <sincerelybr@gmail.com>
2020-03-12 23:48:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,9 +23,8 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese = array.each_with_index{ |item, index| 
-  index += 1 
-  puts "#{index} #{item}" }
+  cheese = cheese_types.each_with_index{ |item, index| 
+  puts "#{item}" }
   array.include? cheese_types
 end
 
