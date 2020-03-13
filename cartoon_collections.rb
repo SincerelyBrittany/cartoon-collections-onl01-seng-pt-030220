3e313ef7fdaea1d2dc8ae4813829739a5b4af952
Brittany Russell <sincerelybr@gmail.com>
2020-03-13 00:06:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  potentially_cheesy_items.find do |maybe_cheese|
+  array.find do |maybe_cheese|
     cheeses.include?(maybe_cheese)
   end
 end
